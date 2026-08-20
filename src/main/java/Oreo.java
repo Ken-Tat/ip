6314074827db75bb9599168f567ceb7852f4f205
Ken-Tat <196010913+Ken-Tat@users.noreply.github.com>
2026-08-20 18:33:40 +0800

    Updated Initial Code to greet the user and exit

--- src/main/java/Oreo.java
+++ src/main/java/Oreo.java
@@ -1,10 +1,23 @@
 public class Oreo {
+    private static final String NAME = "Oreo";
+
     public static void main(String[] args) {
         String banner = "  OOO   RRRR   EEEEE  OOO  \n"
                 + " O   O  R   R  E     O   O \n"
                 + " O   O  RRRR   EEEE  O   O \n"
                 + " O   O  R R    E     O   O \n"
                 + "  OOO   R  RR  EEEEE  OOO  \n";
-        System.out.println(banner);
+
+        String Greeting = "____________________________________________ \n"
+                + banner + "\n"
+                + "Hello! I'm " + NAME + ". \n"
+                + "What can I do for you? \n"
+                + "____________________________________________";
+        System.out.println(Greeting);
+
+        String Goodbye = "____________________________________________ \n"
+                + "Bye. Hope to see you again soon! \n"
+                + "____________________________________________ \n";
+        System.out.println(Goodbye);
     }
 }

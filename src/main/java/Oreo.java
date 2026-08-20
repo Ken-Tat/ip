8453fbe20233dc9f727aa09f700f7c40a025e70f
Ken-Tat <196010913+Ken-Tat@users.noreply.github.com>
2026-08-21 00:10:04 +0800

    implemented command echo alongside exit command

--- src/main/java/Oreo.java
+++ src/main/java/Oreo.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Oreo {
     private static final String NAME = "Oreo";
 
@@ -8,16 +10,32 @@ public class Oreo {
                 + " O   O  R R    E     O   O \n"
                 + "  OOO   R  RR  EEEEE  OOO  \n";
 
-        String Greeting = "____________________________________________ \n"
+        String greeting = "____________________________________________ \n"
                 + banner + "\n"
                 + "Hello! I'm " + NAME + ". \n"
                 + "Let's get started shall we? \n"
                 + "____________________________________________";
-        System.out.println(Greeting);
 
-        String Goodbye = "____________________________________________ \n"
+        String goodbye = "____________________________________________ \n"
                 + "Good work. See you nexttime! \n"
                 + "____________________________________________ \n";
-        System.out.println(Goodbye);
+
+        System.out.println(greeting);
+
+        // take in user input
+        Scanner scanner = new Scanner(System.in);
+        String userInput = "";
+
+        while (!userInput.equals("bye")) {
+            userInput = scanner.nextLine();
+
+            if (userInput.equals("bye")) {
+                System.out.println(goodbye);
+            } else {
+                System.out.println("____________________________________________ \n"
+                            + userInput + " \n"
+                            + "____________________________________________ \n");
+            }
+        }
     }
 }

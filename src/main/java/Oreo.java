ee72dd628db2b338be7c3c86052ce48b8597eab1
Ken-Tat <196010913+Ken-Tat@users.noreply.github.com>
2026-08-21 00:40:37 +0800

    Added Lists logic and task storage

--- src/main/java/Oreo.java
+++ src/main/java/Oreo.java
@@ -1,5 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
+/**
+ * A simple command-line chatbot that stores and displays user-entered tasks.
+ */
 public class Oreo {
     private static final String NAME = "Oreo";
 
@@ -17,12 +22,14 @@ public class Oreo {
                 + "____________________________________________";
 
         String goodbye = "____________________________________________ \n"
-                + "Good work. See you nexttime! \n"
+                + "Good work. See you next time! \n"
                 + "____________________________________________ \n";
 
         System.out.println(greeting);
 
-        // take in user input
+        List<String> tasks = new ArrayList<>();
+
+        // Reads commands from standard input.
         Scanner scanner = new Scanner(System.in);
         String userInput = "";
 
@@ -31,10 +38,23 @@ public class Oreo {
 
             if (userInput.equals("bye")) {
                 System.out.println(goodbye);
+            } else if (userInput.equals("list")) {
+                System.out.println("____________________________________________");
+
+                if (tasks.isEmpty()) {
+                    System.out.println("No tasks in the list.");
+                } else {
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println((i + 1) + ". " + tasks.get(i));
+                    }
+                }
+
+                System.out.println("____________________________________________");
             } else {
                 System.out.println("____________________________________________ \n"
-                            + userInput + " \n"
+                            + "added: " + userInput + " \n"
                             + "____________________________________________ \n");
+                tasks.add(userInput);
             }
         }
     }

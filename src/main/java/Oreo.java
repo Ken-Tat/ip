c5973a2d90c53b52405fb581997853cc7c981985
Ken-Tat <196010913+Ken-Tat@users.noreply.github.com>
2026-08-20 17:57:28 +0800

    Changed name to Oreo

---
+++ src/main/java/Oreo.java
@@ -0,0 +1,10 @@
+public class Oreo {
+    public static void main(String[] args) {
+        String banner = "  OOO   RRRR   EEEEE  OOO  \n"
+                + " O   O  R   R  E     O   O \n"
+                + " O   O  RRRR   EEEE  O   O \n"
+                + " O   O  R R    E     O   O \n"
+                + "  OOO   R  RR  EEEEE  OOO  \n";
+        System.out.println(banner);
+    }
+}
